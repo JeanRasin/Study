dc6d5682965e8a297887b5b634927bf17ef85929
DESKTOP-9D6870A\Jean <logun2@yandex.ru>
2021-01-12 12:14:22 +0500

    Record other

--- Records/Records/Program.cs
+++ Records/Records/Program.cs
@@ -6,6 +6,19 @@ namespace Records
     {
         static void Main(string[] args)
         {
+            // Коротка форма (*позиционная запись*) так же содержит деконструктор.
+            var shortFormRecord = new ShortFormRecord("Asad", 45);
+            var (sfName, sfAge) = shortFormRecord;
+            Console.WriteLine($"Name: {sfName}, Age: {sfAge}");
+
+            Console.WriteLine(new string('-', 10));
+
+            // Создает полнуюкопию.
+            ShortFormRecord cloneSFR = shortFormRecord with { };
+            Console.WriteLine($"Clone shortFormRecord: {cloneSFR}");
+
+            Console.WriteLine(new string('-', 10));
+
             var person1 = new Person() { Name = "Tom", Age = 33 };
             // person1.Name = "Ivan"; error
             var person2 = new Person() { Name = "Tom", Age = 33 };
@@ -34,6 +47,13 @@ namespace Records
 
         }
     }
+
+    // Коротка форма.
+    public record ShortFormRecord(string Name, int Age);
+
+    // Наследование в короткой форме.
+    public sealed record ShortFormRecordHeir(string Name, int Age, string Adress) 
+        : ShortFormRecord(Name, Age);
 
     public class Person
     {
